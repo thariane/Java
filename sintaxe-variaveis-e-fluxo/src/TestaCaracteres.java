25eaf74c6ce0f8d7a31903377fa209355eed2c15
Thainá Ariane <98915321+thariane@users.noreply.github.com>
2023-02-16 22:16:10 -0300

    Add files via upload

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,16 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String [] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		String palavra = "alura cursos";
+		System.out.println(palavra);
+		
+		String parcela1 = "10";
+		String parcela2 = "20";
+
+		System.out.println(parcela1 + parcela2);
+	}
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,19 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args ) {
+		
+		double salario;
+		salario = 1250.71;
+		System.out.println("meu salario: " + salario);
+	
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novatentativa = 5.0 / 2;
+		System.out.println(novatentativa);
+	}
+}
